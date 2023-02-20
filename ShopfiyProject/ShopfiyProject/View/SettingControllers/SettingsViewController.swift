a84d4fa35fc240bb482cc1be671ccf7dd23e75d8
mariam-moataz <mariammansour556@gmail.com>
2023-02-20 21:35:21 +0200

    settings main UI

--- ShopfiyProject/ShopfiyProject/View/SettingControllers/SettingsViewController.swift
+++ ShopfiyProject/ShopfiyProject/View/SettingControllers/SettingsViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    @IBOutlet weak var view: UIView!
-    @IBOutlet weak var tableView: UITableView!
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+   
+    
 
 }
