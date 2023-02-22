b317d984c52522d074f0f666957e3c63cc16d7fd
asmaa <asmaaabdelfattah491@yahoo.com>
2023-02-22 11:03:19 +0200

    SplashScreen

--- ShopfiyProject/ShopfiyProject/ViewController.swift
+++ ShopfiyProject/ShopfiyProject/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var splashImg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.splashImg.image = UIImage(imageLiteralResourceName: "app")
+       
+        
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+           
+           UIView.transition(with: self.splashImg,
+                             duration: 2.0,
+                             options: .transitionCrossDissolve,
+                             animations: {
+                               self.splashImg.image = UIImage(imageLiteralResourceName: "app")
+           }, completion: nil)
+    }
 }
 
