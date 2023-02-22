f78a8dc2f2481879a3fa64ae6379c4d7f4160d7b
mariam-moataz <mariammansour556@gmail.com>
2023-02-22 23:13:53 +0200

    updating the ui in all screen in secondStoryboard

---
+++ ShopfiyProject/ShopfiyProject/View/AddressViewControllers/AddressTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  AddressTableViewCell.swift
+//  ShopfiyProject
+//
+//  Created by Mariam Moataz on 22/02/2023.
+//
+
+import UIKit
+
+class AddressTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
