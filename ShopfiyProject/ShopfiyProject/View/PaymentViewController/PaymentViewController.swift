6d6dcd59f505233111c68c1092f6d779db169d18
mariam-moataz <mariammansour556@gmail.com>
2023-02-25 15:14:55 +0200

    update payment view by adding a collection view for items and scroll view

--- ShopfiyProject/ShopfiyProject/View/PaymentViewController/PaymentViewController.swift
+++ ShopfiyProject/ShopfiyProject/View/PaymentViewController/PaymentViewController.swift
@@ -9,30 +9,22 @@ import UIKit
 
 class PaymentViewController: UIViewController {
 
-    
-    
-    @IBOutlet weak var paymentSegment: UISegmentedControl!
-   
-    
     @IBOutlet weak var countryLabel: UILabel!
-    
-    
     @IBOutlet weak var cityLabel: UILabel!
-    
-    
     @IBOutlet weak var phoneLabel: UILabel!
-    
     @IBOutlet weak var subTotalLabel: UILabel!
-    
     @IBOutlet weak var shippingFeesLabel: UILabel!
     @IBOutlet weak var couponTxtField: UITextField!
     @IBOutlet weak var discountLabel: UILabel!
     @IBOutlet weak var grandTotalLabel: UILabel!
     
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     @IBAction func validateBtn(_ sender: Any) {
@@ -40,5 +32,29 @@ class PaymentViewController: UIViewController {
     
     @IBAction func placeOrderBtn(_ sender: Any) {
     }
+    
+}
+
+extension PaymentViewController : UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width:collectionView.frame.size.width
+                , height: collectionView.frame.size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PaymentCollectionViewCell
+        
+        return cell
+    }
+    
     
 }
