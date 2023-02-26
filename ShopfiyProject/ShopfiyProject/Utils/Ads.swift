7d135256b6e9bd206d2ab68a141432428eb56b15
asmaa <asmaaabdelfattah491@yahoo.com>
2023-02-26 21:28:12 +0200

    custom tab bar and wait in home screen

--- ShopfiyProject/ShopfiyProject/Utils/Ads.swift
+++ ShopfiyProject/ShopfiyProject/Utils/Ads.swift
@@ -8,13 +8,14 @@
 import Foundation
 
 func getAds() -> [AdsDetials]{
+
     let ad1 = AdsDetials(code: "summerOffer",discount: 50, image: "ads1")
     let ad2 = AdsDetials(code: "newOffer",discount: 50, image: "ads2")
     let ad3 = AdsDetials(code: "t-shirtsOffer",discount: 50, image: "ads3")
     let ad4 = AdsDetials(code: "fashionSaleOffer",discount: 50, image: "ads4")
     
     var ads : [AdsDetials] = []
-    
+   
     ads.append(ad1)
     ads.append(ad2)
     ads.append(ad3)
