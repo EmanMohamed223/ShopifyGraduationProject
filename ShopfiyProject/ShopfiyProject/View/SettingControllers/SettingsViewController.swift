fc2fd228b1a8a9a8dee9827e537f132c64aad0a9
asmaa <asmaaabdelfattah491@yahoo.com>
2023-03-02 01:25:19 +0200

    Brand Test and update ui

--- ShopfiyProject/ShopfiyProject/View/SettingControllers/SettingsViewController.swift
+++ ShopfiyProject/ShopfiyProject/View/SettingControllers/SettingsViewController.swift
@@ -40,7 +40,7 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func logoutBtn(_ sender: Any) {
-//        UserDefaultsManager.shared.setUserStatus(userIsLogged: false)
+      UserDefaultsManager.shared.setUserStatus(userIsLogged: false)
 //        print ("logout")
     }
     
