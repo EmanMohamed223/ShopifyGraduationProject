8e6bfebcc63eb546835085b9aaaed0792dfba67f
mariam-moataz <mariammansour556@gmail.com>
2023-03-03 05:19:07 +0200

    post address in api

--- ShopfiyProject/ShopfiyProject/Model/AddressModel/AddressModel.swift
+++ ShopfiyProject/ShopfiyProject/Model/AddressModel/AddressModel.swift
@@ -7,8 +7,20 @@
 
 import Foundation
 
-struct AddressModel{
+struct AddressModel : Codable{
+    var customerId : Int?
     var country : String?
     var city : String?
     var street : String?
+}
+
+struct Customer_addressResponseModel : Codable{
+    var customer_address : Customer_address?
+}
+
+struct Customer_address : Codable{
+    //var customer_id : Int?
+    var country : String?
+    var city : String?
+    var address1 : String?
 }
